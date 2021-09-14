984426285a69e290c19ae6bbb4e32e40f1c2c953
zawnet-pc <zawnet@hotmail.com>
2021-09-14 19:17:21 +0200

    Implementacja sortowania przez wstawianie

--- javabydg11092021/src/Zadania/Zadanie25.java
+++ javabydg11092021/src/Zadania/Zadanie25.java
@@ -1,5 +1,7 @@
 package Zadania;
 
+import java.util.Arrays;
+
 /**
  * Zaimplementuj algorytm sortowania przez wstawianie
  */
@@ -8,22 +10,20 @@ public class Zadanie25 {
     public static void main(String[] args) {
         int a[] = {54,26,93,17,77,31,44,55,20};
 
-        insertSort()
+        System.out.printf(Arrays.toString(insertSort(a)));
     }
 
     private static int[] insertSort(int a[]){
-        int n = a.length-1;
-        for(int i = 1; i<n; i++)
-        {
-            int insertelement = a[i];
-            int j = i-1;
-            while (j>0 && a[j]>insertelement){
-                    int tmp = a[j];
-                    a[i]=a[j];
-                    a[j] = insertelement;
-
-
-            }
+        int n = a.length;
+        int j,v;
+        for(int i = 1; i<n; i++) {
+           j=i;
+           v=a[i];
+           while (j > 0 && a[j-1] > v){
+               a[j] = a[j-1];
+               j = j-1;
+           }
+           a[j] =v;
         }
         return a;
     }
