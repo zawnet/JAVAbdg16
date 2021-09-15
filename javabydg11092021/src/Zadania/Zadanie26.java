21a89eff255ed6865230e9b7441d34bd6e93180c
zawnet-pc <zawnet@hotmail.com>
2021-09-15 19:59:38 +0200

    Dodanie implementacji rekurencyjnej i iteraecyjnej metody sprawdzajacej czy słowo jest palindromem

--- javabydg11092021/src/Zadania/Zadanie26.java
+++ javabydg11092021/src/Zadania/Zadanie26.java
@@ -1,2 +1,34 @@
-package Zadania;public class Zadanie26 {
+package Zadania;
+
+import java.util.Arrays;
+
+/**
+ * Zaimplementuj algorytm sortowania bąelkowego
+ */
+
+public class Zadanie26 {
+
+    public static void main(String[] args) {
+        int a[] = {54,26,93,17,77,31,44,55,20};
+
+        System.out.println(Arrays.toString(bubbleSort(a)));
+    }
+
+    private static int[] bubbleSort(int a[]){
+        int n = a.length;
+
+
+        while (n>0) {
+            for (int i = 0; i < n - 1; i++) {
+                if (a[i] > a[i + 1]) {
+                    int tmp = a[i + 1];
+                    a[i + 1] = a[i];
+                    a[i] = tmp;
+                }
+            }
+            n--;
+        }
+        return a;
+    }
+
 }
--- javabydg11092021/src/Zadania/Zadanie27.java
+++ javabydg11092021/src/Zadania/Zadanie27.java
@@ -1,2 +1,61 @@
-package Zadania;public class Zadanie27 {
+package Zadania;
+/**
+ *
+ * Napisz program, który wykorzystujc rekurencję sprawdzi,
+ * czy wprowadzone przez uytkownika słowo jest palindromem.
+ */
+public class Zadanie27 {
+
+    public static void main(String[] args) {
+        String word = "geuieg";
+        System.out.println(isPalindrome(word));
+        System.out.println(isPalindromeIter(word));
+    }
+
+    /**
+     * Rekurencyjna werjsa metody sprawdzającej czy słowo jest palindromem
+     * @param word sprawdzane słowo
+     * @return
+     */
+    private static boolean isPalindrome(String word){
+        //Ustalamy dlugosc napisu
+        int n = word.length()-1;
+
+        if(word.length()==1){
+            return true;
+        }
+        //w przeciwnym przypadku zwracamy false
+        if (word.charAt(0) != word.charAt(n)) {
+            return false;
+        }
+        //Wywołujemy ponownie metodne na podsłowie 2 do n
+        return isPalindrome(word.substring(2, n));
+
+    }
+
+    /**
+     * Iteracyjna metoda sprawdzajaca czy dane słowo jest palindromem
+     *
+     * @param word
+     * @return wartość logidzna true jak jest false jak nie jest
+     */
+    private static boolean isPalindromeIter(String word){
+        boolean ispalindrome  = false;
+        int n = word.length()-1;
+
+        if(n>0) {
+            for (int i = 0; i < n; i++) {
+                //System.out.println(word.charAt(i) + " "+word.charAt(n));
+                if (word.charAt(i) == word.charAt(n)) {
+                    ispalindrome = true;
+                } else {
+                    ispalindrome = false;
+                    break;
+                }
+                n--;
+            }
+        }
+
+        return ispalindrome;
+    }
 }
