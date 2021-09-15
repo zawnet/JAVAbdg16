9014fd6cf16aa1f11736a618ff17511371a3fa3b
Paweł Zawada <zawnet@hotmail.com>
2021-09-16 00:57:27 +0200

    modyfikacja implementacji Quicksort

--- javabydg11092021/src/Zadania/Zadanie30.java
+++ javabydg11092021/src/Zadania/Zadanie30.java
@@ -10,35 +10,36 @@ public class Zadanie30 {
     public static void main(String[] args) {
         int a[] = {54,26,93,17,77,31,44,55,20};
 
-        System.out.println(Arrays.toString(quickSort(a,0,a.length-1)));
+        quickSort(a,0,a.length-1);
+
+        System.out.println(Arrays.toString(a));
     }
 
-    private static int[] quickSort(int a[], int l, int r){
-        //if(r <= l) return a;
+    private static void quickSort(int a[], int l, int r){
+        if(l>=r) return ;
 
-        int v = a[(l+r)/2]; //ustalamy element rodzielajacy na wyniku dzielania calkowitego dlugosic tablicy przez 2
-        int i = l;
-        int j = r;
+        int v = a[(l+r)/2]; //ustalamy element rodzielajacy (pivot) na wyniku dzielania calkowitego dlugosic tablicy przez 2
+        int i = l-1;
+        int j = r+1;
 
         do{
-            while ( (a[i] < v)) i++;
-            while ( (a[j] > v)) j--;
+            while ( (a[++i] < v)) ;    //dopoki lewa strona jest mniejsza od pivota przesuwaj wskaznik i do przodu
+            while ( (a[--j] > v)) ;    //dopoki prawa strona jest wieksa od pivota przesuwaj wskaznik j do tyłu
 
-            if(i<=j){
+                                        //po wykonaniu tych petli otrzymamy indeks po lewjej stronei z elementem wiekszym od pivota
+                                        // po prawej stronie z elementem mniejszym od pivota
+            if(i<=j){                   // jeżeli wzkańnik j
                 int tmp = a[i];
                 a[i] = a[j];
                 a[j] = tmp;
-                i++; j--;
             }
             else {
-                break;
+                break;              //jezeli po prawej stronie nie znalezmismy ani jednego elementu mnieszego od pivot to przerwij petle
             }
-        } while (i<=j);
+        } while (true);
 
-        if(l<j) return quickSort(a,l,j);
-        if(i<r) return quickSort(a,i,r);
+        if(j>l)  quickSort(a,l,j); //jezeli wskaznij po prawej jest wiekszy od poczaktu to wywolaj na prametrach poczatek do to cojest po prawej
+        if(i<r)  quickSort(a,i,r); //jezeli wskaznik po lewej jest mniejszy niz koniec to wywolaj na parametrahc to co jest po lewej do koniec
 
-
-        return a;
     }
 }
