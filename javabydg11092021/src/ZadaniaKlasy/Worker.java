4411ab0cf8a02b7982ffb97234da0669db5a2912
zawnet-pc <zawnet@hotmail.com>
2021-09-16 21:30:21 +0200

    Zadanie 33 Implementacja klasy Worker kora dziedziczy po Person

--- javabydg11092021/src/ZadaniaKlasy/Worker.java
+++ javabydg11092021/src/ZadaniaKlasy/Worker.java
@@ -1,2 +1,54 @@
-package ZadaniaKlasy;public class Worker {
+package ZadaniaKlasy;
+
+import java.util.Scanner;
+
+/**
+ * Na bazie zadania 32 napisz program, w którym zostanie zawarty proces dziedziczenia. Napisz klas o nazwie Worker, która bdzie
+ * dziedziczy po klasie Person oraz bdzie zawiera dodatkowe pola: education i workplace. Metody init() i print() powinny, podobnie jak dla
+ * klasy Person, obsugiwa wprowadzanie danych i ich wywietlanie.
+ */
+
+public class Worker extends Person{
+    private String education;
+    private String workplace;
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public String getWorkplace() {
+        return workplace;
+    }
+
+    public void setWorkplace(String workplace) {
+        this.workplace = workplace;
+    }
+
+    @Override
+    public void init() {
+        super.init();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Education: ");
+        setEducation(input.nextLine());
+        System.out.println("Workplace: ");
+        setWorkplace(input.nextLine());
+    }
+
+    @Override
+    public void print() {
+        super.print();
+        System.out.println("Education: "+getEducation());
+        System.out.println("Workspace: "+getWorkplace());
+
+    }
+
+    public static void main(String[] args) {
+        Worker worker = new Worker();
+        worker.init();
+        worker.print();
+    }
 }
